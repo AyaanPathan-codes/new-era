2945ab1193a0f43f8d3793bf404d0fe5d8cd488f
AyaanPathan-codes <ayaanpathanra@gmail.com>
2024-10-08 19:36:39 +0530

    release 2

---
+++ register.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyChKRBwAXnXH0MHzhhirAp7Ls55yd7ZCS4",
+    authDomain: "the-new-era-e1877.firebaseapp.com",
+    projectId: "the-new-era-e1877",
+    storageBucket: "the-new-era-e1877.appspot.com",
+    messagingSenderId: "1050919526592",
+    appId: "1:1050919526592:web:296caeacc3439640bfcd25",
+    measurementId: "G-KQNWLQGH0M"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>   
